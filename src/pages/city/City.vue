d01003ab1b7debae10b7c911ee184f15a14812c1
song <max__8888@163.com>
2020-08-03 11:49:21 +0800

    the first time

---
+++ src/pages/city/City.vue
@@ -0,0 +1,29 @@
+
+<template>
+    <div>
+        <CityHeader></CityHeader>
+        <Search></Search>
+        <List></List>
+        <Alphabet></Alphabet>
+    </div>
+</template>
+
+<script>
+    import CityHeader from "./components/CityHeader"
+    import Search from "./components/Search"
+    import List from "./components/List"
+    import Alphabet from "./components/Alphabet"
+    export default {
+        name : "City",
+        components:{
+            CityHeader,
+            Search,
+            List,
+            Alphabet
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,36 @@
+
+<template>
+    <ul class="list">
+		<li class="item">A</li>
+		<li class="item">B</li>
+		<li class="item">C</li>
+		<li class="item">D</li>
+		<li class="item">E</li>
+	</ul>
+</template>
+
+<script>
+    export default {
+        name:"Alphabet"
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+@import '~styles/varibles.styl'
+.list
+        display: flex
+        flex-direction: column
+        justify-content: center
+        position: absolute
+        top: 1.58rem
+        right: 0
+        bottom: 0
+        width: .44rem
+        // background :red
+        .item
+            line-height: .4rem
+            text-align: center
+            color: $bgColor
+
+</style>
